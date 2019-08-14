cc299a6b39b19b3d8d37ba9938961794329f60b9
shikakun <shikakun@shikakun.com>
2019-08-14 11:59:49 +0900

    Parcel でバンドリングできるようにした

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue/dist/vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        example: 'Yo'
+      };
+    }
+  });
+});
